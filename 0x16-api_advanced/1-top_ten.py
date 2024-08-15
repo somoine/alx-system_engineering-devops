a6a5d2fb00a4712446a62fca7d65c0d68700f469
somoine <isampaiyo8@gmail.com>
2024-08-15 12:15:33 +0300

    ff

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-""" Script to get the first 10 hot
-    posts on Reddit
+"""prints the titles of the first 10 hot posts listed for a given subreddit.
+
 """
-from requests import get
+import requests
 
 
 def top_ten(subreddit):
-    """get first 10 hot post for a subreddit"""
-    if subreddit and type(subreddit) is str:
-        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-        headers = {'user-agent': 'my-app/0.0.1'}
-        params = {'limit': 10}
-        req = get(url, params=params, headers=headers, allow_redirects=False)
-        if req.status_code == 200:
-            data = req.json()
-            posts = data.get('data', {}).get('children', {})
-            for post in posts:
-                print(post.get('data').get('title'))
-        else:
-            print(None)
+    '''Return the title of the first 10 hot posts
+    listed for a given subreddit'''
+    try:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0\
+            (by /u/Large_Alternative_30)",
+        }
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if (response.status_code == 404):
+            print('None')
+            return 0
+        request = response.json().get('data').get('children')
+        for i in range(10):
+            print(request[i].get('data').get('title'))
+    except Exception:
+        print('None')
+        return 0
